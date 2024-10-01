d3194bcbf46b73bc8771971039e49b96f60e62ca
bing <99920799+biiiing-lab@users.noreply.github.com>
2024-10-01 10:15:54 +0900

    Delete CH05 directory

--- CH05/src/main/java/org/example/ch05/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package org.example.ch05.repository;
-
-import org.example.ch05.domain.Article;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ArticleRepository extends JpaRepository<Article, Long> {
-}
